ffd75e5004ba62d689ca9dc38c89ff0ed907669f
Tim Berners-Lee <timbl+git@w3.org>
2017-12-30 19:23:43 +0000

    standard defaultPane.js . removed unused code

--- defaultPane.js
+++ defaultPane.js
@@ -8,112 +8,67 @@
 var UI = require('solid-ui')
 
 module.exports = {
-    icon:  UI.icons.originalIconBase + 'about.png',
+  icon: UI.icons.originalIconBase + 'about.png',
 
-    name: 'default',
+  name: 'default',
 
-    label: function(subject) { return 'about ';},
+  label: function (subject) { return 'about ' },
 
-    render: function(subject, dom) {
-
-      var filter = function(pred, inverse) {
-        if (typeof UI.panes.internal.predicates[pred.uri] !== 'undefined')
-            return false;
-        if (inverse && (pred.uri ==
-                "http://www.w3.org/1999/02/22-rdf-syntax-ns#type")) return false;
-        return true;
+  render: function (subject, dom) {
+    var filter = function (pred, inverse) {
+      if (typeof UI.panes.internal.predicates[pred.uri] !== 'undefined') {
+        return false
       }
-
-
-        //var doc = dom.wrappedJSObject;   Jim? why-tim
-        // dump( doc );
-        var outliner = UI.panes.getOutliner(dom)
-        var kb = UI.store;
-        var outline = outliner; //@@
-        UI.log.info("@defaultPane.render, dom is now " + dom.location);
-        subject = kb.canon(subject);
-        var div = dom.createElement('div')
-        //var f = jq("<div></div>", doc);
-        //jq(div, doc).append(f);
-        //f.resource({subject:"http://web.mit.edu/jambo/www/foaf.rdf#jambo", predicate:"http://xmlns.com/foaf/0.1/knows"});
-        div.setAttribute('class', 'defaultPane')
-//        appendRemoveIcon(div, subject, div);
-
-        var plist = kb.statementsMatching(subject)
-        outliner.appendPropertyTRs(div, plist, false, filter)
-        plist = kb.statementsMatching(undefined, undefined, subject)
-        outliner.appendPropertyTRs(div, plist, true, filter)
-        if ((subject.termType == 'Literal') && (subject.value.slice(0,7) == 'http://'))
-            outliner.appendPropertyTRs(div,
-                [$rdf.st(kb.sym(subject.value), UI.ns.link('uri'), subject)],
-                true, filter)
-        if ((subject.termType == 'NamedNode' &&
-             kb.updater.editable(UI.rdf.Util.uri.docpart(subject.uri), kb))
-             || (subject.termType == 'BlankNode'
-                && kb.anyStatementMatching(subject)
-                && kb.anyStatementMatching(subject).why
-                && kb.anyStatementMatching(subject).why.uri
-                && kb.updater.editable(kb.anyStatementMatching(subject).why.uri)
-                //check the document containing something about of the bnode @@ what about as object?
-             /*! && HCIoptions["bottom insert highlights"].enabled*/)) {
-            var holdingTr = dom.createElement('tr'); //these are to minimize required changes
-            var holdingTd = dom.createElement('td'); //in userinput.js
-            holdingTd.setAttribute('colspan','2');
-            holdingTd.setAttribute('notSelectable','true');
-            var img = dom.createElement('img');
-            img.src = UI.icons.originalIconBase + 'tango/22-list-add-new.png';
-            img.addEventListener('click', function  add_new_tripleIconMouseDownListener(e) {
-                    outliner.UserInput.addNewPredicateObject(e);
-                    e.stopPropagation();
-                    e.preventDefault();
-                    return;
-            });
-            img.className='bottom-border-active';
-            //img.addEventListener('click', thisOutline.UserInput.addNewPredicateObject,false);
-            div.appendChild(holdingTr).appendChild(holdingTd).appendChild(img);
-        }
-        return div
-    },
-
-    sync: function(subject, dom, div) { // Untested  and not the best way to do it
-    // This code was cut out of outline.js
-    //    best way is to leave TRs there and add/delete any necessray extras
-
-        UI.log.info('Re-expand: '+div)
-        // try{table.replaceChild(expandedHeaderTR(subject),table.firstChild)}
-        // catch(e){}   // kludge... Todo: remove this (seeAlso UserInput::clearInputAndSave)
-        var row, s
-        var expandedNodes = {}
-        var parent = div
-        for (row = parent.firstChild; row; row = row.nextSibling) { // Note which p,o pairs are exppanded
-            if (row.childNodes[1]
-                && row.childNodes[1].firstChild.nodeName == 'TABLE') {
-                s = row.AJAR_statement
-                if (!expandedNodes[s.predicate.toString()]) {
-                    expandedNodes[s.predicate.toString()] = {}
-                }
-                expandedNodes[s.predicate.toString()][s.object.toString()] =
-                    row.childNodes[1].childNodes[1]
-            }
-        }
-
-        var table = propertyTable(subject, undefined, pane)  // Re-build table
-
-        for (row = table.firstChild; row; row = row.nextSibling) {
-            s = row.AJAR_statement
-            if (s) {
-                if (expandedNodes[s.predicate.toString()]) {
-                    var node =
-                        expandedNodes[s.predicate.toString()][s.object.toString()]
-                    if (node) {
-                        row.childNodes[1].replaceChild(node,
-                                        row.childNodes[1].firstChild)
-                    }
-                }
-            }
-        }
-        // do some other stuff here
+      if (inverse && (pred.uri ===
+        'http://www.w3.org/1999/02/22-rdf-syntax-ns#type')) return false
+      return true
     }
-};
+
+    var outliner = UI.panes.getOutliner(dom)
+    var kb = UI.store
+    // var outline = outliner; // @@
+    UI.log.info('@defaultPane.render, dom is now ' + dom.location)
+    subject = kb.canon(subject)
+    var div = dom.createElement('div')
+    div.setAttribute('class', 'defaultPane')
+    //        appendRemoveIcon(div, subject, div)
+
+    var plist = kb.statementsMatching(subject)
+    outliner.appendPropertyTRs(div, plist, false, filter)
+    plist = kb.statementsMatching(undefined, undefined, subject)
+    outliner.appendPropertyTRs(div, plist, true, filter)
+    if ((subject.termType === 'Literal') && (subject.value.slice(0, 7) === 'http://')) {
+      outliner.appendPropertyTRs(div,
+        [$rdf.st(kb.sym(subject.value), UI.ns.link('uri'), subject)],
+        true, filter)
+    }
+    if ((subject.termType === 'NamedNode' &&
+      kb.updater.editable(UI.rdf.Util.uri.docpart(subject.uri), kb)) ||
+        (subject.termType === 'BlankNode' &&
+        kb.anyStatementMatching(subject) &&
+        kb.anyStatementMatching(subject).why &&
+        kb.anyStatementMatching(subject).why.uri &&
+        kb.updater.editable(kb.anyStatementMatching(subject).why.uri)
+      // check the document containing something about of the bnode @@ what about as object?
+      /*! && HCIoptions["bottom insert highlights"].enabled*/)) {
+      var holdingTr = dom.createElement('tr') // these are to minimize required changes
+      var holdingTd = dom.createElement('td') // in userinput.js
+      holdingTd.setAttribute('colspan', '2')
+      holdingTd.setAttribute('notSelectable', 'true')
+      var img = dom.createElement('img')
+      img.src = UI.icons.originalIconBase + 'tango/22-list-add-new.png'
+      img.addEventListener('click', function add_new_tripleIconMouseDownListener (e) {
+        outliner.UserInput.addNewPredicateObject(e)
+        e.stopPropagation()
+        e.preventDefault()
+        return
+      })
+      img.className = 'bottom-border-active'
+      // img.addEventListener('click', thisOutline.UserInput.addNewPredicateObject,false)
+      div.appendChild(holdingTr).appendChild(holdingTd).appendChild(img)
+    }
+    return div
+  }
+}
 
 // ends
