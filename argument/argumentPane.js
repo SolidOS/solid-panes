16f86e397fcc5977cb14aa2df90552148d282a50
Tim Berners-Lee <timbl+git@w3.org>
2017-12-31 14:16:21 -0500

    standard argumentPane.js (provisional placeholder not finsihed pane)

--- argument/argumentPane.js
+++ argument/argumentPane.js
@@ -2,7 +2,12 @@
 **
 **  This pane shows a position and optionally the positions which
 ** support or oppose it.
+** @@ Unfinsihed.
+** Should allow editing the data too
+
 */
+
+const UI = require('solid-ui')
 
 // console.log('@@@ argument pane icon at ' + (module.__dirname || __dirname) + '/icon_argument.png')
 module.exports = {
@@ -20,33 +25,38 @@ module.exports = {
   },
 
   // View the data in a file in user-friendly way
-  render: function (subject, myDocument) {
+  render: function (subject, dom) {
     var outliner = UI.panes.getOutliner(dom)
-    var $r = UI.rdf
     var kb = UI.store
     var arg = UI.ns.arg
 
     subject = kb.canon(subject)
     var types = kb.findTypeURIs(subject)
 
-    var div = myDocument.createElement('div')
+    var div = dom.createElement('div')
     div.setAttribute('class', 'argumentPane')
 
     // var title = kb.any(subject, UI.ns.dc('title'))
 
     var comment = kb.any(subject, UI.ns.rdfs('comment'))
     if (comment) {
-      var para = myDocument.createElement('p')
+      var para = dom.createElement('p')
       para.setAttribute('style', 'margin-left: 2em; font-style: italic;')
       div.appendChild(para)
       para.textContent = comment.value
     }
+
+    div.appendChild(dom.createElement('hr'))
+
     var plist = kb.statementsMatching(subject, arg('support'))
     outliner.appendPropertyTRs(div, plist, false)
 
-    var plist = kb.statementsMatching(subject, arg('opposition'))
+    div.appendChild(dom.createElement('hr'))
+
+    plist = kb.statementsMatching(subject, arg('opposition'))
     outliner.appendPropertyTRs(div, plist, false)
 
+    div.appendChild(dom.createElement('hr'))
     return div
   }
 }
