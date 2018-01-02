8fcf6e51196a26ba5d2336c256e443b89550b01e
Tim Berners-Lee <timbl+git@w3.org>
2018-01-02 14:58:41 -0500

    standard video/videoPane.js

--- video/videoPane.js
+++ video/videoPane.js
@@ -3,41 +3,31 @@
 */
 var UI = require('solid-ui')
 
-
-module.exports =  {
+module.exports = {
 
   icon: UI.icons.iconBase + 'noun_1619.svg',
 
   name: 'video',
 
   // Does the subject deserve an slideshow pane?
-  label: function(subject) {
-    var kb = UI.store;
-    var ns = UI.ns;
-    var typeURIs = kb.findTypeURIs(subject);
-
+  label: function (subject) {
+    var kb = UI.store
+    var typeURIs = kb.findTypeURIs(subject)
     var prefix = $rdf.Util.mediaTypeClass('video/*').uri.split('*')[0]
     for (var t in typeURIs) {
-      if (t.startsWith(prefix)) return "Play video"
+      if (t.startsWith(prefix)) return 'Play video'
     }
 
-    return null;
+    return null
   },
 
-
-  render: function(subject, dom) {
-
-    var kb = UI.store;
-    var ns = UI.ns;
-    var div = dom.createElement("div")
-
+  render: function (subject, dom) {
+    var div = dom.createElement('div')
     var video = div.appendChild(dom.createElement('video'))
     video.setAttribute('controls', 'yes')
     video.setAttribute('src', subject.uri)
     video.setAttribute('width', '100%')
-
-    return div;
+    return div
   }
 }
-
-//ends
+// ends
