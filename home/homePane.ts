8fda2537461a6d48ea444172f1c230618328214a
Vincent <git@vincenttunru.com>
2019-05-14 12:50:33 +0200

    Apply StandardJS linter to TypeScript

--- home/homePane.ts
+++ home/homePane.ts
@@ -8,7 +8,7 @@
 **
 */
 
-import { PaneDefinition } from '../types';
+import { PaneDefinition } from '../types'
 import UI from 'solid-ui'
 import panes from 'pane-registry'
 
@@ -21,33 +21,33 @@ const HomePane: PaneDefinition = {
   //
   //   yes, always!
   //
-  label: function (subject) {
+  label: function () {
     return 'home'
   },
 
   render: function (subject, dom) {
     var showContent = function () {
-      var context = {div: div, dom: dom, statusArea: div, me: me}
+      var context = { div: div, dom: dom, statusArea: div, me: me }
 
       div.appendChild(dom.createElement('h4')).textContent = 'Login status'
       var loginStatusDiv = div.appendChild(dom.createElement('div'))
       // TODO: Find out what the actual type is:
       type UriType = unknown;
-      loginStatusDiv.appendChild(UI.authn.loginStatusBox(dom, (uri: UriType) => {
+      loginStatusDiv.appendChild(UI.authn.loginStatusBox(dom, () => {
         // Here we know new log in status
       }))
 
       div.appendChild(dom.createElement('h4')).textContent = 'Create new thing somewhere'
       var creationDiv = div.appendChild(dom.createElement('div'))
-      var creationContext = {div: creationDiv, dom: dom, statusArea: div, me: me}
+      var creationContext = { div: creationDiv, dom: dom, statusArea: div, me: me }
       UI.create.newThingUI(creationContext, panes) // newUI Have to pass panes down
 
       div.appendChild(dom.createElement('h4')).textContent = 'Private things'
       // TODO: Replace by a common, representative interface
       type AuthContext = unknown;
-      UI.authn.registrationList(context, {private: true}).then(function (context: AuthContext) {
+      UI.authn.registrationList(context, { private: true }).then(function (context: AuthContext) {
         div.appendChild(dom.createElement('h4')).textContent = 'Public things'
-        UI.authn.registrationList(context, {public: true}).then(function (context: AuthContext) {
+        UI.authn.registrationList(context, { public: true }).then(function () {
           // done
         })
       })
@@ -63,4 +63,4 @@ const HomePane: PaneDefinition = {
 } // pane object
 
 // ends
-export default HomePane;
+export default HomePane
--- typings/solid-ui/index.d.ts
+++ typings/solid-ui/index.d.ts
@@ -1,6 +1,6 @@
 declare module 'solid-ui' {
-  export type SolidUi = any;
-  const solidUi: SolidUi;
+  export type SolidUi = any
+  const solidUi: SolidUi
 
-  export default solidUi;
+  export default solidUi
 }
