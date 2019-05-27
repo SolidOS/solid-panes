fbe4bcd57917a3b230e26dbe8494d5aa59e39de7
Vincent <git@vincenttunru.com>
2019-05-27 13:50:40 +0200

    Dynamically detect package names
    Note that getPanes() can only be used server-side. To actually load
    them, the server should generate client-side scripts that actually
    include the pane, preferably without already including the pane's
    code in the initial payload.

--- packages/panes/getPanes.js
+++ packages/panes/getPanes.js
@@ -1,7 +1,10 @@
 const path = require('path')
+const fs = require('fs')
 
 function getPanes () {
-  const paneNames = ['scratchpad']
+  const paneNames = fs.readdirSync(__dirname)
+    .filter(fileOrDir => fs.statSync(path.join(__dirname, fileOrDir)).isDirectory())
+    .filter(directory => directory !== 'webpack-config-panes')
 
   const panes = paneNames.map(name => ({ name: name, path: getPaneDir(name) }))
 
