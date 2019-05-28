9d068cce92a80b6290249a8e7af798b82820168e
Vincent <git@vincenttunru.com>
2019-05-28 16:36:46 +0200

    Convert the profile pane to TypeScript

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ register(require('./sharing/sharingPane.js'))
 register(require('./internalPane.js'))
 // The home pane is a 2016 experiment. Always there.
 
-register(require('./profile/profilePane.js')) // edit your public profile
+register(require('./profile/profilePane').default) // edit your public profile
 register(require('./trustedApplications/trustedApplicationsPane').default) // manage your trusted applications
 register(require('./home/homePane').default)
 
