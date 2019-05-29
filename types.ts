c612c59b1ff8eb7a269857d604ed774c60b5e241
Vincent <git@vincenttunru.com>
2019-05-29 11:58:14 +0200

    Add missing properties on PaneDefinition type

--- types.ts
+++ types.ts
@@ -1,11 +1,27 @@
-import { Node } from 'rdflib'
+import { NamedNode, IndexedFormula } from 'rdflib'
 
 // Note: there might be a more appropriate project to hold this definition:
 export interface PaneDefinition {
   icon: string;
   name: string;
-  label: (subject: Node) => string | null;
-  render: (subject: Node, dom: HTMLDocument, options?: unknown) => HTMLElement;
-  shouldGetFocus?: (subject: Node) => boolean;
+  label: (subject: NamedNode) => string | null;
+  render: (subject: NamedNode, dom: HTMLDocument, options?: unknown) => HTMLElement;
+  shouldGetFocus?: (subject: NamedNode) => boolean;
   requireQueryButton?: boolean;
+  mintClass?: NamedNode;
+  mintNew?: (options: NewPaneOptions, store: IndexedFormula) => Promise<NewPaneOptions & { newInstance: NamedNode }>;
 };
+interface NewPaneOptions {
+  appPathSegment: string;
+  div: HTMLDivElement;
+  dom: HTMLDocument;
+  folder: NamedNode;
+  iconEle: HTMLImageElement;
+  me?: NamedNode;
+  newBase: string;
+  newInstance: NamedNode;
+  noIndexHTML: boolean;
+  noun: string;
+  pane: PaneDefinition;
+  refreshTarget: HTMLTableElement;
+}
