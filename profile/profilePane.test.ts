30afaf4daa958be506b38f808b53c925c620be7f
Ted Thibodeau Jr <tthibodeau@openlinksw.com>
2019-06-27 16:29:55 -0400

    make sample URIs use *.example domain

--- profile/profilePane.test.ts
+++ profile/profilePane.test.ts
@@ -8,20 +8,20 @@ const ns = namespaces($rdf as any)
 describe('getLabel', () => {
   it('should return "Edit your profile" by default', () => {
     const mockStore = $rdf.graph()
-    const mockProfile = $rdf.sym('https://mock-profile')
+    const mockProfile = $rdf.sym('https://profile.example')
     expect(getLabel(mockProfile, mockStore, ns)).toBe('Edit your profile')
   })
 
   it('should return "Your profile" when viewing a Person', () => {
     const mockStore = $rdf.graph()
-    const mockProfile = $rdf.sym('https://mock-profile')
+    const mockProfile = $rdf.sym('https://profile.example')
     mockStore.add(mockProfile, ns.rdf('type'), ns.foaf('Person'), mockProfile.doc())
     expect(getLabel(mockProfile, mockStore, ns)).toBe('Your Profile')
   })
 
   it('should return "Your profile" when viewing an Individual', () => {
     const mockStore = $rdf.graph()
-    const mockProfile = $rdf.sym('https://mock-profile')
+    const mockProfile = $rdf.sym('https://profile.example')
     mockStore.add(mockProfile, ns.rdf('type'), ns.vcard('Individual'), mockProfile.doc())
     expect(getLabel(mockProfile, mockStore, ns)).toBe('Your Profile')
   })
