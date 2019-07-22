bb99960599125c356799c1d3787288158c2d2357
Vincent <git@vincenttunru.com>
2019-07-22 15:10:30 +0200

    Allow adding a contact through the Dashboard

--- dashboard/datasister-dashboard/widgets/Contacts.tsx
+++ dashboard/datasister-dashboard/widgets/Contacts.tsx
@@ -8,12 +8,31 @@ import { ProfileBadge } from '../components/ProfileBadge'
 const ns = namespaces($rdf)
 
 export const ContactsWidget: React.FC = () => {
-  const { store, fetcher } = React.useContext(DataBrowserContext)
+  const { store, fetcher, updater } = React.useContext(DataBrowserContext)
+  const webId = useWebId()
 
-  const contacts = useContacts(store, fetcher)
+  const storedContacts = useContacts(store, fetcher)
+  const [addedContacts, addContact] = React.useReducer<React.Reducer<string[], string>>(
+    (previouslyAdded, newContact) => previouslyAdded.concat(newContact),
+    []
+  )
 
-  if (!Array.isArray(contacts) || contacts.length === 0) {
-    return null
+  const contacts = (storedContacts || []).concat(addedContacts)
+  function onAddContact (contactWebId: string) {
+    if (!webId || !contactWebId) {
+      return
+    }
+
+    const profile = $rdf.sym(webId)
+    updater.update(
+      [],
+      [$rdf.st(profile, ns.foaf('knows'), $rdf.sym(contactWebId), profile.doc())],
+      (_uri, success, _errorBody) => {
+        if (success) {
+          addContact(contactWebId)
+        }
+      }
+    )
   }
 
   return (
@@ -22,11 +41,46 @@ export const ContactsWidget: React.FC = () => {
         <h2 className="title">Contacts</h2>
         <div className="content">
           <ul>
-            {contacts.map((contact) => <ProfileBadge key={contact} webId={contact}/>)}
+            {contacts.map((contact) => <li key={contact}><ProfileBadge webId={contact}/></li>)}
           </ul>
         </div>
+        <h3>Add a contact</h3>
+        <WebIdForm onSubmit={onAddContact}/>
       </section>
     </div>
+  )
+}
+
+const WebIdForm: React.FC<{ onSubmit: (webId: string) => void }> = (props) => {
+  const [webId, setWebId] = React.useState<string>('')
+
+  function handleSubmit (event: React.FormEvent) {
+    event.preventDefault()
+
+    props.onSubmit(webId)
+    setWebId('')
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="field">
+        <label className="label">
+          WebID:
+          <input
+            type="url"
+            onChange={(e) => setWebId(e.target.value)}
+            value={webId}
+            placeholder="https://www.w3.org/People/Berners-Lee/card#i"
+            name="webid"
+            id="webid"
+            className="input"
+          />
+        </label>
+      </div>
+      <div className="control">
+        <input type="submit" value="Add" className="button"/>
+      </div>
+    </form>
   )
 }
 
