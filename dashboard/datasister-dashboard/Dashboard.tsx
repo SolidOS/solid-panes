0244ff129f61c65cac9f7fa9fd86c1cc36295141
Vincent <git@vincenttunru.com>
2019-07-23 16:35:45 +0200

    Remove mock Profile Widget

--- dashboard/datasister-dashboard/Dashboard.tsx
+++ dashboard/datasister-dashboard/Dashboard.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { ProfileWidget } from './widgets/Profile'
 import { ContactsWidget } from './widgets/Contacts'
 import { BookmarksWidget } from './widgets/Bookmarks'
 import { FolderWidget } from './widgets/Folder'
@@ -11,9 +10,6 @@ export const Dashboard: React.FC<{
     <>
       <section className="section">
         <div className="columns grid ids-container__four-column">
-          <div className="column item">
-            <ProfileWidget/>
-          </div>
           <div className="column item">
             <ContactsWidget/>
           </div>
