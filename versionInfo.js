025b76888945e5df74ed991cc80fe145340dbdd9
Arne Hassel <arne.hassel@gmail.com>
2019-08-06 18:15:19 +0200

    Upgraded deps

--- versionInfo.js
+++ versionInfo.js
@@ -1,25 +1,21 @@
 module.exports = {
-buildTime: "2019-08-06T11:42:02Z",
-commit: "272c6dfe3b3a109c852670b53a0bb1a6ad03ba1a",
+buildTime: "2019-08-06T16:13:55Z",
+commit: "ac6c128b1cefc1b92aca883ef356122fd2e5c181",
 npmInfo:
-{
-  'solid-panes': '1.3.16-0',
-  npm: '6.10.0',
-  ares: '1.15.0',
-  brotli: '1.0.7',
-  cldr: '35.1',
+{ 'solid-panes': '1.3.16-1',
+  npm: '6.4.1',
+  ares: '1.10.1-DEV',
+  cldr: '32.0',
   http_parser: '2.8.0',
-  icu: '64.2',
-  llhttp: '1.1.4',
-  modules: '72',
+  icu: '60.1',
+  modules: '57',
   napi: '4',
-  nghttp2: '1.39.1',
-  node: '12.7.0',
-  openssl: '1.1.1c',
-  tz: '2019a',
-  unicode: '12.1',
-  uv: '1.30.1',
-  v8: '7.5.288.22-node.16',
-  zlib: '1.2.11'
-}
+  nghttp2: '1.33.0',
+  node: '8.16.0',
+  openssl: '1.0.2r',
+  tz: '2017c',
+  unicode: '10.0',
+  uv: '1.23.2',
+  v8: '6.2.414.77',
+  zlib: '1.2.11' }
 };
