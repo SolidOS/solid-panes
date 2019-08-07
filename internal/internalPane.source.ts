22d48361b233347060cee5d7d4c22c39fa2de499
Arne Hassel <arne.hassel@gmail.com>
2019-08-07 15:07:44 +0200

    Fixing linting errors

--- internal/internalPane.source.ts
+++ internal/internalPane.source.ts
@@ -5,62 +5,62 @@
 */
 /* global alert confirm */
 
-import { PaneDefinition } from "../types"
-import UI from "solid-ui"
-import panes from "pane-registry"
-import { IndexedFormula, NamedNode } from "rdflib"
+import { PaneDefinition } from '../types'
+import UI from 'solid-ui'
+import panes from 'pane-registry'
+import { IndexedFormula, NamedNode } from 'rdflib'
 
 const ns = UI.ns
 
 const pane: PaneDefinition = {
-  icon: UI.icons.originalIconBase + "tango/22-emblem-system.png",
+  icon: UI.icons.originalIconBase + 'tango/22-emblem-system.png',
 
-  name: "internal",
+  name: 'internal',
 
-  label: function() {
-    return "under the hood" // There is often a URI even of no statements
+  label: function () {
+    return 'under the hood' // There is often a URI even of no statements
   },
 
-  render: function(subject, dom) {
+  render: function (subject, dom) {
     var $r = UI.rdf
     var kb = UI.store
     subject = kb.canon(subject)
     var types = kb.findTypeURIs(subject)
 
     function filter (pred: NamedNode) {
-      if (types["http://www.w3.org/2007/ont/link#ProtocolEvent"]) return true // display everything for them
-      return !!(typeof (panes as any).internal.predicates[pred.uri] !== "undefined")
+      if (types['http://www.w3.org/2007/ont/link#ProtocolEvent']) return true // display everything for them
+      return !!(typeof (panes as any).internal.predicates[pred.uri] !== 'undefined')
     }
 
-    var div = dom.createElement("div")
-    div.setAttribute("class", "internalPane")
-    div.setAttribute("style", "background-color: #ddddff; padding: 0.5em; border-radius: 1em;")
+    var div = dom.createElement('div')
+    div.setAttribute('class', 'internalPane')
+    div.setAttribute('style', 'background-color: #ddddff; padding: 0.5em; border-radius: 1em;')
 
     function deleteRecursive (kb: IndexedFormula, folder: NamedNode) {
       const fetcher = (kb as any).fetcher
       if (!fetcher) {
-        console.error("No fetcher available")
+        console.error('No fetcher available')
         return
       }
-      return new Promise(function(resolve, reject) {
-        fetcher.load(folder).then(function() {
-          let promises = kb.each(folder, ns.ldp("contains")).map(file => {
-            if (kb.holds(file, ns.rdf("type"), ns.ldp("BasicContainer"))) {
+      return new Promise(function (resolve, reject) {
+        fetcher.load(folder).then(function () {
+          let promises = kb.each(folder, ns.ldp('contains')).map(file => {
+            if (kb.holds(file, ns.rdf('type'), ns.ldp('BasicContainer'))) {
               return deleteRecursive(kb, file as NamedNode)
             } else {
-              console.log("deleteRecursive leaf file: " + file)
-              return fetcher.webOperation("DELETE", (file as NamedNode).uri)
+              console.log('deleteRecursive leaf file: ' + file)
+              return fetcher.webOperation('DELETE', (file as NamedNode).uri)
             }
           })
-          Promise.all(promises).then(res => {
-            console.log("deleteRecursive empty folder: " + folder)
-            fetcher.webOperation("DELETE", folder.uri)
+          Promise.all(promises).then(() => {
+            console.log('deleteRecursive empty folder: ' + folder)
+            fetcher.webOperation('DELETE', folder.uri)
               .then(() => {
-                console.log("Deleted Ok: " + folder)
+                console.log('Deleted Ok: ' + folder)
                 resolve()
               })
               .catch((err: string) => {
-                var str = "Unable to delete " + folder + ": " + err
+                var str = 'Unable to delete ' + folder + ': ' + err
                 console.log(str)
                 reject(new Error(str))
               })
@@ -73,49 +73,49 @@ const pane: PaneDefinition = {
       })
     }
 
-    const isDocument = subject.uri && !subject.uri.includes("#")
+    const isDocument = subject.uri && !subject.uri.includes('#')
     if (isDocument) {
-      const controls = div.appendChild(dom.createElement("table"))
-      controls.style.width = "100%"
-      controls.style.margin = "1em"
-      const controlRow = controls.appendChild(dom.createElement("tr"))
+      const controls = div.appendChild(dom.createElement('table'))
+      controls.style.width = '100%'
+      controls.style.margin = '1em'
+      const controlRow = controls.appendChild(dom.createElement('tr'))
 
-      const deleteCell = controlRow.appendChild(dom.createElement("td"))
-      const isFolder = ((subject.uri && subject.uri.endsWith("/")) ||
-        kb.holds(subject, ns.rdf("type"), ns.ldp("Container")))
-      const noun = isFolder ? "folder" : "file"
+      const deleteCell = controlRow.appendChild(dom.createElement('td'))
+      const isFolder = ((subject.uri && subject.uri.endsWith('/')) ||
+        kb.holds(subject, ns.rdf('type'), ns.ldp('Container')))
+      const noun = isFolder ? 'folder' : 'file'
       if (!isProtectedUri(subject)) {
         console.log(subject)
-        var deleteButton = UI.widgets.deleteButtonWithCheck(dom, deleteCell, noun, function() {
-          if (!confirm("Are you sure you want to delete " + subject + "? This cannot be undone.")) return
+        var deleteButton = UI.widgets.deleteButtonWithCheck(dom, deleteCell, noun, function () {
+          if (!confirm('Are you sure you want to delete ' + subject + '? This cannot be undone.')) return
           var promise = isFolder ? deleteRecursive(kb, subject)
-            : kb.fetcher.webOperation("DELETE", subject.uri)
+            : kb.fetcher.webOperation('DELETE', subject.uri)
           promise
             .then(() => {
-              var str = "Deleted: " + subject
+              var str = 'Deleted: ' + subject
               console.log(str)
             })
             .catch((err: any) => {
-              var str = "Unable to delete " + subject + ": " + err
+              var str = 'Unable to delete ' + subject + ': ' + err
               console.log(str)
               alert(str)
             })
         })
-        deleteButton.style = "height: 2em;"
-        deleteButton.class = "" // Remove hover hide
+        deleteButton.style = 'height: 2em;'
+        deleteButton.class = '' // Remove hover hide
         deleteCell.appendChild(deleteButton)
       }
 
-      const refreshCell = controlRow.appendChild(dom.createElement("td"))
-      const refreshButton = UI.widgets.button(dom, UI.icons.iconBase + "noun_479395.svg", "refresh")
+      const refreshCell = controlRow.appendChild(dom.createElement('td'))
+      const refreshButton = UI.widgets.button(dom, UI.icons.iconBase + 'noun_479395.svg', 'refresh')
       refreshCell.appendChild(refreshButton)
-      refreshButton.addEventListener("click", () => {
-        kb.fetcher.refresh(subject, function(ok: boolean, errm: string) {
+      refreshButton.addEventListener('click', () => {
+        kb.fetcher.refresh(subject, function (ok: boolean, errm: string) {
           let str
           if (ok) {
-            str = "Refreshed OK: " + subject
+            str = 'Refreshed OK: ' + subject
           } else {
-            str = "Error refreshing: " + subject + ": " + errm
+            str = 'Error refreshing: ' + subject + ': ' + errm
           }
           console.log(str)
           alert(str)
@@ -127,15 +127,15 @@ const pane: PaneDefinition = {
     var docURI = null
     if (subject.uri) {
       plist.push($r.st(subject,
-        kb.sym("http://www.w3.org/2007/ont/link#uri"), subject.uri, UI.store.fetcher.appNode))
-      if (subject.uri.indexOf("#") >= 0) {
-        docURI = subject.uri.split("#")[0]
+        kb.sym('http://www.w3.org/2007/ont/link#uri'), subject.uri, UI.store.fetcher.appNode))
+      if (subject.uri.indexOf('#') >= 0) {
+        docURI = subject.uri.split('#')[0]
         plist.push($r.st(subject,
-          kb.sym("http://www.w3.org/2007/ont/link#documentURI"),
-          subject.uri.split("#")[0], UI.store.fetcher.appNode))
+          kb.sym('http://www.w3.org/2007/ont/link#documentURI'),
+          subject.uri.split('#')[0], UI.store.fetcher.appNode))
         plist.push($r.st(subject,
-          kb.sym("http://www.w3.org/2007/ont/link#document"),
-          kb.sym(subject.uri.split("#")[0]), UI.store.fetcher.appNode))
+          kb.sym('http://www.w3.org/2007/ont/link#document'),
+          kb.sym(subject.uri.split('#')[0]), UI.store.fetcher.appNode))
       } else {
         docURI = subject.uri
       }
@@ -144,7 +144,7 @@ const pane: PaneDefinition = {
       var ed = UI.store.updater.editable(docURI)
       if (ed) {
         plist.push($r.st(subject,
-          kb.sym("http://www.w3.org/ns/rww#editable"),
+          kb.sym('http://www.w3.org/ns/rww#editable'),
           kb.literal(ed), UI.store.fetcher.appNode))
       }
     }
@@ -156,36 +156,36 @@ const pane: PaneDefinition = {
   },
 
   predicates: { // Predicates used for inner workings. Under the hood
-    "http://www.w3.org/2007/ont/link#request": 1,
-    "http://www.w3.org/2007/ont/link#requestedBy": 1,
-    "http://www.w3.org/2007/ont/link#source": 1,
-    "http://www.w3.org/2007/ont/link#session": 2, // 2=  test neg but display
-    "http://www.w3.org/2007/ont/link#uri": 1,
-    "http://www.w3.org/2007/ont/link#documentURI": 1,
-    "http://www.w3.org/2007/ont/link#document": 1,
-    "http://www.w3.org/2007/ont/link#all": 1, // From userinput.js
-    "http://www.w3.org/2007/ont/link#Document": 1,
-    "http://www.w3.org/ns/rww#editable": 1,
-    "http://www.w3.org/2000/01/rdf-schema#seeAlso": 1,
-    "http://www.w3.org/2002/07/owl#": 1
+    'http://www.w3.org/2007/ont/link#request': 1,
+    'http://www.w3.org/2007/ont/link#requestedBy': 1,
+    'http://www.w3.org/2007/ont/link#source': 1,
+    'http://www.w3.org/2007/ont/link#session': 2, // 2=  test neg but display
+    'http://www.w3.org/2007/ont/link#uri': 1,
+    'http://www.w3.org/2007/ont/link#documentURI': 1,
+    'http://www.w3.org/2007/ont/link#document': 1,
+    'http://www.w3.org/2007/ont/link#all': 1, // From userinput.js
+    'http://www.w3.org/2007/ont/link#Document': 1,
+    'http://www.w3.org/ns/rww#editable': 1,
+    'http://www.w3.org/2000/01/rdf-schema#seeAlso': 1,
+    'http://www.w3.org/2002/07/owl#': 1
   },
 
   classes: { // Things which are inherently already undercover
-    "http://www.w3.org/2007/ont/link#ProtocolEvent": 1
+    'http://www.w3.org/2007/ont/link#ProtocolEvent': 1
   }
 }
 
 function isProtectedUri (subject: NamedNode): boolean {
   // TODO: Could make the code below smarter by removing some of the redundancy by creating a recursive function, but did not bother now
   const siteUri = subject.site().uri
-  return subject.uri === siteUri
-    || subject.uri === siteUri + "profile/"
-    || subject.uri === siteUri + "profile/card"
-    || subject.uri === siteUri + "settings/"
-    || subject.uri === siteUri + "settings/prefs.ttl"
-    || subject.uri === siteUri + "settings/privateTypeIndex.ttl"
-    || subject.uri === siteUri + "settings/publicTypeIndex.ttl"
-    || subject.uri === siteUri + "settings/serverSide.ttl"
+  return subject.uri === siteUri ||
+    subject.uri === siteUri + 'profile/' ||
+    subject.uri === siteUri + 'profile/card' ||
+    subject.uri === siteUri + 'settings/' ||
+    subject.uri === siteUri + 'settings/prefs.ttl' ||
+    subject.uri === siteUri + 'settings/privateTypeIndex.ttl' ||
+    subject.uri === siteUri + 'settings/publicTypeIndex.ttl' ||
+    subject.uri === siteUri + 'settings/serverSide.ttl'
 }
 
 export default pane
