a5ab3825e33888110a160f0f35047f01cbfa7ee1
Arne Hassel <arne.hassel@gmail.com>
2019-08-29 15:41:54 +0200

    Removing scratchPad as planned
    Was marked for removal in https://team.inrupt.net/public/mashlib//Panes%20(developer%20and%20power%20user%20mode).pdf

--- scratchpad/pane.ts
+++
@@ -1,9 +0,0 @@
-import { RevampPaneDefinition } from '../types'
-import { isPad, getTitle } from './data'
-import { view as scratchpadView } from './view'
-
-export const pane: RevampPaneDefinition = {
-  canHandle: (subject, store) => isPad(subject, store),
-  label: (subject, store) => getTitle(store, subject),
-  view: scratchpadView
-}
--- scratchpad/types.ts
+++
@@ -1,3 +0,0 @@
-import { IndexedFormula, NamedNode } from 'rdflib'
-
-export type InitialisationFunction = (store: IndexedFormula, user?: NamedNode) => Promise<NamedNode>;
