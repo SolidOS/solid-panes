443d12de32925ca703a232c5797bfd8ba48ff6df
Arne Hassel <arne.hassel@gmail.com>
2019-09-02 13:46:33 +0200

    Updated versionInfo for new release

--- versionInfo.js
+++ versionInfo.js
@@ -1,23 +1,25 @@
 module.exports = {
-buildTime: "2019-08-20T14:34:34Z",
-commit: "856c7d6d6b33468410d0ae904bf780167b8f34be",
+buildTime: "2019-09-02T11:46:10Z",
+commit: "3ebd4a586110e4e71a06862df80179a0d485f82b",
 npmInfo:
-{ 'solid-panes': '1.4.0',
-  npm: '6.7.0',
+{
+  'solid-panes': '2.0.0',
+  npm: '6.10.0',
   ares: '1.15.0',
   brotli: '1.0.7',
-  cldr: '34.0',
+  cldr: '35.1',
   http_parser: '2.8.0',
-  icu: '63.1',
-  llhttp: '1.1.1',
-  modules: '67',
+  icu: '64.2',
+  llhttp: '1.1.4',
+  modules: '72',
   napi: '4',
-  nghttp2: '1.34.0',
-  node: '11.10.1',
-  openssl: '1.1.1a',
-  tz: '2018e',
-  unicode: '11.0',
-  uv: '1.26.0',
-  v8: '7.0.276.38-node.17',
-  zlib: '1.2.11' }
+  nghttp2: '1.39.1',
+  node: '12.7.0',
+  openssl: '1.1.1c',
+  tz: '2019a',
+  unicode: '12.1',
+  uv: '1.30.1',
+  v8: '7.5.288.22-node.16',
+  zlib: '1.2.11'
+}
 };
