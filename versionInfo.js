2fff2f32be0116b9f9c65e76cc3cf0d7357c2f4a
Vincent <git@vincenttunru.com>
2019-10-22 12:15:01 +0200

    Ensure the current latest version 1.0.6 of rdflib
    Also include the latest version of solid-ui that also uses that
    version of rdflib, and a couple of bugfixes.

--- versionInfo.js
+++ versionInfo.js
@@ -1,25 +1,25 @@
 module.exports = {
-buildTime: "2019-09-24T15:02:42Z",
-commit: "e97a3c894e971d01c5194ac0ad10948f6da3069e",
+buildTime: "2019-10-22T10:14:10Z",
+commit: "8c3850664d55cf1c65bb68cf992f7071ba9acc12",
 npmInfo:
 {
   'solid-panes': '2.0.2',
-  npm: '6.10.0',
+  npm: '6.9.0',
   ares: '1.15.0',
   brotli: '1.0.7',
   cldr: '35.1',
   http_parser: '2.8.0',
   icu: '64.2',
-  llhttp: '1.1.4',
+  llhttp: '1.1.1',
   modules: '72',
   napi: '4',
-  nghttp2: '1.39.1',
-  node: '12.7.0',
-  openssl: '1.1.1c',
+  nghttp2: '1.38.0',
+  node: '12.1.0',
+  openssl: '1.1.1b',
   tz: '2019a',
   unicode: '12.1',
-  uv: '1.30.1',
-  v8: '7.5.288.22-node.16',
+  uv: '1.28.0',
+  v8: '7.4.288.21-node.16',
   zlib: '1.2.11'
 }
 };
