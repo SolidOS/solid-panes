93c6d258bde3f962b095957b0da6f58d3107dd55
Angelo Veltens <angelo.veltens@codecentric.de>
2021-02-28 13:28:49 +0100

    pass solid-logic to panes

--- src/outline/context.ts
+++ src/outline/context.ts
@@ -1,17 +1,21 @@
 import { DataBrowserContext, LiveStore, PaneRegistry } from 'pane-registry'
 import { getOutliner } from '../index'
+import { solidLogicSingleton } from 'solid-ui'
+import { SolidLogic } from 'solid-logic'
 
 export function createContext (
   dom: HTMLDocument,
   paneRegistry: PaneRegistry,
-  store: LiveStore
+  store: LiveStore,
+  logic: SolidLogic
 ): DataBrowserContext {
   return {
     dom,
     getOutliner,
     session: {
       paneRegistry,
-      store
+      store,
+      logic
     }
   }
 }
