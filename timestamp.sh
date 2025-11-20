ba2f46dbe7fcfd87fdbe762c8f37d0611a9a5c0d
timea-solid <4144203+timea-solid@users.noreply.github.com>
2025-11-20 17:16:39 +0100

    fix timestamp script

--- timestamp.sh
+++ timestamp.sh
@@ -2,7 +2,11 @@
 
 echo "export default {"
 date -u '+buildTime: "%Y-%m-%dT%H:%M:%SZ",'
-git log | grep commit | head -1 | sed -e 's/ /: "/' | sed -e 's/$/",/'
+if [ -d .git ]; then
+  git log | grep commit | head -1 | sed -e 's/ /: "/' | sed -e 's/$/",/'
+else
+  echo 'commit: "unknown",'
+fi
 echo "  npmInfo: {"
   npm version | sed 's/\x1b\[[0-9;:]*[mG]//g' | grep -v '^{' | while read line; do
     key=$(echo "$line" | cut -d ':' -f 1 | tr -d ' ')
