166e72b9e466e510c40ab79ad987041cb3746056
Sharon Stratsianis <sstratsianis@gmail.com>
2026-09-23 12:11:35 +1000

    fix some styling and remove dialog from display

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,6 @@ import {
 } from 'pane-registry'
 import { createContext } from './outline/context'
 import { initMainPage, refreshUI } from './mainPage'
-import './toberemoved/ItemsAndTheirTicketsDialog'
 
 function getOutliner (dom, environment?: RenderEnvironment): OutlineManager {
   if (!dom.outlineManager) {
